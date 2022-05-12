13b05afad2fbe2b010cf2d6ec047cbc75a03fc95
Ferve <ferve91@gmail.com>
2022-05-12 20:42:08 -0300

    Termine toda la pagination, falta agregar algunos retoques pero por los menos es funcional

--- rick-and-morty-app/src/App.js
+++ rick-and-morty-app/src/App.js
@@ -1,18 +1,31 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import Characters from "./components/Characters";
+import Pagination from "./components/Pagination";
 
 function App() {
   const [characters, setcharacters] = useState([]);
+  const [info, setinfo] = useState({});
 
   const initialUrl = "https://rickandmortyapi.com/api/character";
 
   const fetchCharacters = (initialUrl) => {
     fetch(initialUrl)
       .then((response) => response.json())
-      .then((data) => setcharacters(data.results))
+      .then((data) => {
+        setcharacters(data.results);
+        setinfo(data.info);
+      })
       .catch((error) => console.log(error));
   };
+
+  const onPrevious = () => {
+    fetchCharacters(info.prev);
+  }
+
+  const onNext = () => {
+    fetchCharacters(info.next);
+  }
 
   useEffect(() => {
     fetchCharacters(initialUrl);
@@ -21,8 +34,10 @@ function App() {
   return (
     <>
       <Navbar brand="Rick and Morty App" />
-      <div className="container">
-        <Characters characters={characters}/>
+      <div className="container mt-5">
+        <Pagination prev={info.prev} next={info.next} onPrevious={onPrevious} onNext={onNext}/>
+        <Characters characters={characters} />
+        <Pagination prev={info.prev} next={info.next} onPrevious={onPrevious} onNext={onNext}/>
       </div>
     </>
   );
--- rick-and-morty-app/src/components/Characters.js
+++ rick-and-morty-app/src/components/Characters.js
@@ -4,9 +4,15 @@ export const Characters = ({ characters }) => {
   return (
     <div className="row">
       {characters.map((item, index) => (
-        <div key={index} className="col">
+        <div key={index} className="col mb-4">
           <div className="card" style={{minWidth: "200px"}}>
             <img src={item.image} alt="" />
+            <div className="card-body">
+                <h5 className="card-title">{item.name}</h5>
+                <hr />
+                <p>Especie: {item.species}</p>
+                <p>Location: {item.location.name}</p>
+            </div>
           </div>
         </div>
       ))}
